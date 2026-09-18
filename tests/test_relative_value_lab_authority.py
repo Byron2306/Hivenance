73747118c18910ed70d2ae2fc0383b51ae4d0800
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-19 00:47:42 +0200

    test: bind entrainment to research authority lock

--- tests/test_relative_value_lab_authority.py
+++ tests/test_relative_value_lab_authority.py
@@ -21,6 +21,7 @@ COMPONENTS = (
     "relative_value_harmony_law",
     "relative_value_waggle_protocol",
     "relative_value_musical_cognition",
+    "relative_value_polyphonic_entrainment",
     "relative_value_market_hunting",
     "relative_value_colony_correlation",
     "relative_value_ml_challenger",

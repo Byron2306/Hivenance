553f3891e03fff6ae4b941e7bdbb3af5736b0e83
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-19 00:56:43 +0200

    test: bind queen and epoch to research authority lock

--- tests/test_relative_value_lab_authority.py
+++ tests/test_relative_value_lab_authority.py
@@ -22,6 +22,8 @@ COMPONENTS = (
     "relative_value_waggle_protocol",
     "relative_value_musical_cognition",
     "relative_value_polyphonic_entrainment",
+    "relative_value_governance_epoch",
+    "relative_value_conducting_queen",
     "relative_value_market_hunting",
     "relative_value_colony_correlation",
     "relative_value_ml_challenger",

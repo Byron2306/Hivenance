2dd93ffb1cbe3c40ab15e4bcb4e0ea578bdc66ef
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-19 01:21:13 +0200

    test: bind Mystique to research authority lock

--- tests/test_relative_value_lab_authority.py
+++ tests/test_relative_value_lab_authority.py
@@ -28,6 +28,7 @@ COMPONENTS = (
     "relative_value_causal_cascade",
     "relative_value_hive_pulse",
     "relative_value_polyphonic_resonance",
+    "relative_value_mystique",
     "relative_value_market_hunting",
     "relative_value_colony_correlation",
     "relative_value_ml_challenger",

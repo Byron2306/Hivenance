594ea7a87c302f0d10d956443099c2bf9ae1d54f
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-19 02:05:20 +0200

    test: bind VNS score conductor to research authority

--- tests/test_relative_value_lab_authority.py
+++ tests/test_relative_value_lab_authority.py
@@ -31,6 +31,7 @@ COMPONENTS = (
     "relative_value_mystique",
     "relative_value_cognitive_metabolism",
     "relative_value_world_score",
+    "relative_value_vns_score_conductor",
     "relative_value_market_hunting",
     "relative_value_colony_correlation",
     "relative_value_ml_challenger",

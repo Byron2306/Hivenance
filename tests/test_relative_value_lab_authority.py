384c8fed9f4827e5eeca838b1f0173d30629d10e
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-19 02:56:48 +0200

    test: bind pollen profit lab to research-only authority

--- tests/test_relative_value_lab_authority.py
+++ tests/test_relative_value_lab_authority.py
@@ -35,6 +35,7 @@ COMPONENTS = (
     "relative_value_vns_score_stream",
     "relative_value_polyphonic_quorum",
     "relative_value_pollen_economy",
+    "relative_value_pollen_profit_lab",
     "relative_value_market_hunting",
     "relative_value_colony_correlation",
     "relative_value_ml_challenger",
